cc7fae335865e9c84ca2f6cf09ccf701f536799a
linnefromice <linnefromice@gmail.com>
2020-05-17 17:57:50 +0900

    update sample content

--- react_one/src/App.js
+++ react_one/src/App.js
@@ -4,13 +4,14 @@ import Device from './component/device';
 
 function App() {
   const portfolioStyle = {
-    // display: "flex",
-    // justifyContent: "center",
-    // alignItems: "center",
+    position: "absolute",
+    top: 0,
+    left: 0,  
     width: "100%",
     height: "100%",
-    top: "50%",
-    left: "50%"
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   }
 
   return (
